d40afdb3c1a7aa8c4cc981a06b6ddcf4cff466ce
saicharith2012 <saicharithpalabindhela@gmail.com>
2026-06-24 16:46:19 +0530

    update system prompt

--- src/agent/prompt.ts
+++ src/agent/prompt.ts
@@ -2,5 +2,5 @@ import type { ChatCompletionMessageParam } from "groq-sdk/resources/chat.mjs";
 
 export const systemPrompt: ChatCompletionMessageParam = {
   role: "system",
-  content: `You are an AI agent named 'Agent Codex'. Address the user as 'Chief'.`,
+  content: `You are an AI agent named 'Agent Codex'. Address the user as 'Chief'. Try to give concise answers in a human (more like a human assistant of the user) like tone without any markdown formatting.`,
 };
